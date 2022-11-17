70917c10ad570b09aaf5023fd28f8e2d7783430d
maknarang29 <maknarang29@gmail.com>
2022-11-17 17:17:10 +0530

    Linked list

---
+++ Data Structures/Linked List/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include "LinkedList.cpp"
+#include "Insert.cpp"
+#include "printMiddle.cpp"
+
+
+//Linked List with three nodes
+int main(){
+
+    Node* head = NULL; 
+      
+    // Insert 6. So linked list becomes 6->NULL 
+    append(&head, 6); 
+      
+    // Insert 7 at the beginning. 
+    // So linked list becomes 7->6->NULL 
+    push(&head, 7); 
+      
+    // Insert 1 at the beginning. 
+    // So linked list becomes 1->7->6->NULL 
+    push(&head, 1); 
+      
+    // Insert 4 at the end. So 
+    // linked list becomes 1->7->6->4->NULL 
+    append(&head, 4); 
+      
+    // Insert 8, after 7. So linked 
+    // list becomes 1->7->8->6->4->NULL 
+    addAfter(head->next, 8); 
+      
+    cout<<"Created Linked list is: "; 
+    printList(head); 
+    printMiddle(head);     
+    return 0;
+}
+
