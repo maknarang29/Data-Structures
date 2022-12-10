677a9329f58983e6260b40ab5b2205a19faa871f
maknarang29 <maknarang29@gmail.com>
2022-12-10 22:59:15 +0530

    Trees

--- Data Structures/Tree/traversal.cpp
+++ Data Structures/Tree/traversal.cpp
@@ -9,9 +9,13 @@ Inorder Traversal
 1. Traverse the left subtreei.e call Inorder(left->subtree)
 2. Visit Root
 3. Traverse the right subtreei.e call Inorder(right->subtree)
+
+Inorder [Left Root right]
+Time Complexity O(n)
+Auxillary Space O(H+1)
 */
 
-void printInorder(Node* node){
+void printInorder(TreeNode* node){
     if (!node) return;
     printInorder(node->left);
     cout<<node->data<<" ";  
