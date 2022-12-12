d2792f5130929ba03e0219695368802099102db7
maknarang29 <maknarang29@gmail.com>
2022-12-12 23:25:08 +0530

    traversal pre and post

--- Data Structures/Tree/main.cpp
+++ Data Structures/Tree/main.cpp
@@ -7,12 +7,17 @@ int main()
     TreeNode* root = new TreeNode(1);
     root->left = new TreeNode(2);
     root->right = new TreeNode(3);
-    root->left->left = new TreeNode(4);
-    root->left->right = new TreeNode(5);
+    root->right->left = new TreeNode(4);
+    root->right->right = new TreeNode(5);
  
  
       // Function call
     cout << "\nInorder traversal of binary tree is \n";
     printInorder(root);
+    cout << "\nPreorder traversal of binary tree is \n";
+    printPreorder(root);
+    cout << "\nPostorder traversal of binary tree is \n";
+    printPostorder(root);
+ 
  
 } 
