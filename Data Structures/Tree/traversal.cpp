70c2c7a7c343550ddcf871c01a25b00152a4ef8e
maknarang29 <maknarang29@gmail.com>
2022-12-12 23:26:41 +0530

    traversal

--- Data Structures/Tree/traversal.cpp
+++ Data Structures/Tree/traversal.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+#include <iostream
 #include "Tree.cpp"
 using namespace std;
 
@@ -6,9 +6,9 @@ using namespace std;
 
 /*
 Inorder Traversal
-1. Traverse the left subtreei.e call Inorder(left->subtree)
+1. Traverse the left subtree i.e call Inorder(left->subtree)
 2. Visit Root
-3. Traverse the right subtreei.e call Inorder(right->subtree)
+3. Traverse the right subtree i.e call Inorder(right->subtree)
 
 Inorder [Left Root right]
 Time Complexity O(n)
@@ -20,4 +20,39 @@ void printInorder(TreeNode* node){
     printInorder(node->left);
     cout<<node->data<<" ";  
     printInorder(node->right);
+}
+
+
+/*
+Preorder Traversal
+1. Visit the root
+2. Traverse the left subtree i.e. call preorder(left->subtree) 
+3. Traverse the right subtree i.e. call preorder(right->subtree) 
+
+Preorder [Root Left Right]
+Time Complexity O(n)
+Auxillary Space O(H)
+*/
+void printPreorder( TreeNode* node){
+    if (!node) return;
+    cout<<node->data<<" ";
+    printPreorder(node->left);
+    printPreorder(node->right);
+}
+
+/*
+Postorder Traversal
+1. Traverse the left subtree i.e. call preorder(left->subtree) 
+2. Traverse the right subtree i.e. call preorder(right->subtree) 
+3. Visit the root
+Postorder [Root Left Right]
+Time Complexity θ(n)
+Auxillary Space θ(H)
+*/
+
+void printPostorder(TreeNode* node){
+    if (!node) return;
+    printPreorder(node->left);
+    printPreorder(node->right);
+    cout<<node->data<<" ";
 }
